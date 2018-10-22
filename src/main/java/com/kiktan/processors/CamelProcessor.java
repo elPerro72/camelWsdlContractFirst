cec137eb8b1cf19d74e15c4329d7659e74c9445e
kiktan\elperro <raymon.de.hond@gmail.com>
2018-10-22 20:17:49 +0000

    changed cxf dataFormat to PAYLOAD

--- src/main/java/com/kiktan/processors/CamelProcessor.java
+++ src/main/java/com/kiktan/processors/CamelProcessor.java
@@ -1,23 +1,26 @@
 package com.kiktan.processors;
 
 import java.util.List;
+import java.util.Map;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import com.kiktan.endpoint.InputSOATest;
 import com.kiktan.endpoint.OutputSOATest;
+import org.apache.cxf.binding.soap.SoapHeader;
 
 public class CamelProcessor implements Processor {
 
     public void process(Exchange exchange) throws Exception {
     	
-        OutputSOATest out = new OutputSOATest();
+        //OutputSOATest out = new OutputSOATest();
         
         //Get input from exchange
-        List soaList = exchange.getIn().getBody(List.class);
-        InputSOATest inputSOATest = (InputSOATest) soaList.get(0);
-        out.setResult("Welcome " + inputSOATest.getTest().toString());
+        String body = exchange.getIn().getBody(String.class);
+        Map<String, Object> headers = exchange.getIn().getHeaders();
+        // OutputSOATest ff = (OutputSOATest) soaList.get(0);
+      //  ff.setResult("Welcome ");
         //set output in exchange
-        exchange.getOut().setBody(out);
+        exchange.getOut().setBody((Object)body);
     }
 
 }
--- src/main/java/com/kiktan/processors/MapPayload.java
+++ src/main/java/com/kiktan/processors/MapPayload.java
@@ -2,15 +2,20 @@ package com.kiktan.processors;
 
 import com.kiktan.endpoint.InputSOATest;
 import com.kiktan.endpoint.OutputSOATest;
+import org.apache.camel.Headers;
+import java.util.Map;
 
 import javax.wsdl.Input;
 
 public class MapPayload {
 
-    public static InputSOATest map(com.kiktan.endpoint.InputSOATest custom){
+    public static String map(@Headers Map header, String custom){
 
         OutputSOATest ff = new OutputSOATest();
         ff.setResult("Welcome ");
+
+        InputSOATest fff = new InputSOATest();
+        fff.setTest("kolder");
 
         return custom;
     }
